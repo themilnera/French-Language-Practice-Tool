570375a3aa89944771ca44a6bdb9f989931681a2
themilnera <amilner32@gmail.com>
2026-08-17 13:47:06 -0600

    Style tweaks for the nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -11,22 +11,23 @@
 	//I'm planning to have some settings you can change but there's no use having the settings wheel without them right this moment
 </script>
 
-<div class="flex h-23 justify-center">
-	<div class="color-lighter-indigo flex h-full w-200 items-center justify-between gap-5 md:rounded-b-lg md:border-r-2 md:border-b-2 md:border-l-2 md:border-gray-700!">
-		<div class="xs:text-sm ml-10 flex gap-5 md:text-lg">
+<div class="flex h-23 items-center justify-center">
+	<div class="color-lighter-indigo flex h-full w-200 items-center justify-center gap-5 text-blue-50 md:rounded-b-lg md:border-r-2 md:border-b-2 md:border-l-2 md:border-gray-700!">
+		<div class="xs:text-sm flex gap-5 md:text-lg">
 			<button
 				onclick={() => {
 					setTimeout(() => goto('/study'), 0);
 				}}
-				class={slug == 'study' ? 'color-mid-indigo cursor-pointer rounded-xl border-b-2 p-2 text-shadow-blue-50 hover:text-shadow-xs' : 'cursor-pointer p-2 text-shadow-blue-50 hover:text-shadow-xs'}
+				class={slug == 'study' ? 'color-mid-indigo cursor-pointer rounded-xl border-b-2 p-2 hover:bg-blue-800!' : 'cursor-pointer rounded-xl p-2 text-shadow-blue-900 hover:bg-gray-700!'}
 				>Study Mode</button
 			>
 			<button
 				onclick={() => {
 					setTimeout(() => goto('/test'), 0);
 				}}
-				class={slug == 'test' ? 'color-mid-indigo cursor-pointer rounded-xl border-b-2 p-2 text-shadow-blue-50 hover:text-shadow-xs' : 'cursor-pointer p-2 text-shadow-blue-50 hover:text-shadow-xs'}
-				>Test Mode</button
+				class={slug == 'test'
+					? 'color-mid-indigo cursor-pointer rounded-xl border-b-2 p-2 text-shadow-blue-500 hover:bg-blue-800!'
+					: 'cursor-pointer rounded-xl p-2 text-shadow-blue-900 hover:bg-gray-700'}>Test Mode</button
 			>
 		</div>
 		<!-- <button class="mr-10 w-10 cursor-pointer text-gray-200 hover:text-gray-800"><SettingsIcon /></button> -->
